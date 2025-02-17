f826c02b642bfe4dd965e0cdcdfa99a4cd41b1ff
Aleksandr Makridin <amakridin@corelogic.com>
2025-02-16 18:10:00 -0600

    0003:moved role explanation to a separate file

--- src/main/java/com/makridin/chat_bot/configuration/ApplicationConfiguration.java
+++ src/main/java/com/makridin/chat_bot/configuration/ApplicationConfiguration.java
@@ -1,16 +1,21 @@
 package com.makridin.chat_bot.configuration;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 @Configuration
 public class ApplicationConfiguration {
 
+    @Value("classpath:roleExplanation.txt")
+    Resource resource;
+
     @Bean
     public ChatClient chatClient(ChatClient.Builder builder) {
         return builder
-                .defaultSystem("You are an educational tutor. Explain concepts clearly and simply.")
+                .defaultSystem(resource)
                 .build();
     }
 }
